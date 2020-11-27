6a91bb64ea5fd7427000a3fb78a82b4d1d163dec
t-bolt <isholasobuur@gmail.com>
2020-11-27 20:06:15 +0100

    added viewbinding to my welcome activity

--- app/src/main/java/com/example/hotelreservation/MainActivity.java
+++ app/src/main/java/com/example/hotelreservation/MainActivity.java
@@ -4,11 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
-import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Button;
 
 import java.io.IOException;
 
@@ -18,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//
+
 //        Intent userIntent = new Intent();
 //        CharSequence user_mail = userIntent.getStringExtra("USER_MAIL");
 //
