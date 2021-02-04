d1807fa01442cfc315f417adc2114d47f5346247
t-bolt <isholasobuur@gmail.com>
2021-02-04 15:18:04 +0100

    wired up the fragmentHotelList to its parent activity

--- app/src/main/java/com/example/hotelreservation/HotelListActivity.java
+++ app/src/main/java/com/example/hotelreservation/HotelListActivity.java
@@ -3,16 +3,19 @@ package com.example.hotelreservation;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
 import com.example.hotelreservation.databinding.ActivityHotelListBinding;
 
 public class HotelListActivity extends AppCompatActivity {
 
     private ActivityHotelListBinding binding;
-    int currentIndex = 0;
-    HotelsAndNames[] mHotelsAndNames = new HotelsAndNames[] {
-        new HotelsAndNames(R.drawable.bcksignup, R.string.hotel_1, R.string.hotel_description_1),
-            new HotelsAndNames(R.drawable.amex, R.string.hotel_2, R.string.hotel_description_2)
-    };
+//    int currentIndex = 0;
+//    HotelsAndNames[] mHotelsAndNames = new HotelsAndNames[] {
+//        new HotelsAndNames(R.drawable.bcksignup, R.string.hotel_1, R.string.hotel_description_1),
+//            new HotelsAndNames(R.drawable.amex, R.string.hotel_2, R.string.hotel_description_2)
+//    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +23,22 @@ public class HotelListActivity extends AppCompatActivity {
         binding = ActivityHotelListBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        int myName = mHotelsAndNames[currentIndex].getHotelName();
-        int myHotel = mHotelsAndNames[currentIndex].getHotelPic();
-        int myDescription = mHotelsAndNames[currentIndex].getHotelDescription();
-        binding.txtHotelDescription.setText(myDescription);
-        binding.txtHotelName.setText(myName);
-        binding.imgHotelPic.setImageResource(myHotel);
+//        int myName = mHotelsAndNames[currentIndex].getHotelName();
+//        int myHotel = mHotelsAndNames[currentIndex].getHotelPic();
+//        int myDescription = mHotelsAndNames[currentIndex].getHotelDescription();
+//        binding.txtHotelDescription.setText(myDescription);
+//        binding.txtHotelName.setText(myName);
+//        binding.imgHotelPic.setImageResource(myHotel);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = new HotelListFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
     }
 
 
---
+++ app/src/main/java/com/example/hotelreservation/HotelListFragment.java
@@ -0,0 +1,6 @@
+package com.example.hotelreservation;
+
+import androidx.fragment.app.Fragment;
+
+public class HotelListFragment extends Fragment {
+}
