4aba05d3c92591ffd815c30cdaf8d6d460da420e
t-bolt <isholasobuur@gmail.com>
2021-02-04 21:58:41 +0100

    added a recyclerview as the parent view of the fragment_hotel_list

--- app/src/main/java/com/example/hotelreservation/HotelListFragment.java
+++ app/src/main/java/com/example/hotelreservation/HotelListFragment.java
@@ -1,6 +1,26 @@
 package com.example.hotelreservation;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class HotelListFragment extends Fragment {
+    private RecyclerView mRecyclerView;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_hotel_list, container, false);
+        mRecyclerView = view.findViewById(R.id.hotel_recyclerview);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        return view;
+    }
 }
