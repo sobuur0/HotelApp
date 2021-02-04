d6af6e377f1bec224ccd93569703a1d95bfe193e
t-bolt <isholasobuur@gmail.com>
2021-02-04 22:24:04 +0100

    added an adapter class to communicate between the recyclerview and the viewholder

--- app/src/main/java/com/example/hotelreservation/HotelListFragment.java
+++ app/src/main/java/com/example/hotelreservation/HotelListFragment.java
@@ -11,8 +11,11 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 public class HotelListFragment extends Fragment {
     private RecyclerView mRecyclerView;
+    private HotelAdapter mAdapter;
 
     @Nullable
     @Override
@@ -21,12 +24,46 @@ public class HotelListFragment extends Fragment {
         mRecyclerView = view.findViewById(R.id.hotel_recyclerview);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
+        updateUI();
+
         return view;
+    }
+
+    private void updateUI() {
+        HotelList hotelList = HotelList.get(getActivity());
+        List<HotelsAndNames> hotelsAndNames = hotelList.getHotelsAndNames();
+        mAdapter = new HotelAdapter(hotelsAndNames);
+        mRecyclerView.setAdapter(mAdapter);
     }
 
     private class HotelHolder extends RecyclerView.ViewHolder {
         public HotelHolder(LayoutInflater inflater, ViewGroup parent) {
             super(inflater.inflate(R.layout.list_item, parent, false));
+        }
+    }
+
+    private class HotelAdapter extends RecyclerView.Adapter<HotelHolder> {
+        private List<HotelsAndNames> mHotelsAndNames;
+
+        public HotelAdapter(List<HotelsAndNames> hotelsAndNames) {
+            mHotelsAndNames = hotelsAndNames;
+        }
+
+        @NonNull
+        @Override
+        public HotelHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+            return new HotelHolder(layoutInflater, parent);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull HotelHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return mHotelsAndNames.size();
         }
     }
 }
